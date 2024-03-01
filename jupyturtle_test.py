53f6a64cad87d698412a3090912c919e877ffdee
Luciano Ramalho <luciano@ramalho.org>
2024-03-01 05:21:59 -0300

    fixed tests

--- jupyturtle_test.py
+++ jupyturtle_test.py
@@ -1,46 +1,41 @@
 from jupyturtle import Turtle, Point
 
 
-def test_drawing_exists():
-    # check that we can make a turtle and it has a drawing
+def test_start_position():
     t = Turtle()
-    assert t.drawing.get_SVG('').startswith('<svg')
+    assert t.position == Point(t.drawing.width // 2, t.drawing.height // 2)
 
 
 def test_forward():
     t = Turtle()
-    assert t.lines == []
+    pos0 = t.position
     d = 100
     t.forward(d)
-    p1 = Point(t.drawing.width // 2, t.drawing.height // 2)
-    p2 = Point(p1.x + d, p1.y)
-    assert t.lines[0].p1 == p1
-    assert t.lines[0].p2 == p2
+    pos1 = Point(pos0.x + d, pos0.y)
+    assert t.position == pos1
 
 
 def test_forward_right_square():
     t = Turtle()
-    pos = t.position
-    heading = t.heading
+    pos0 = t.position
+    head0 = t.heading
     d = 42
     sides = 4
     for _ in range(sides):
         t.forward(d)
         t.right(90)
-    assert t.position == pos
-    assert t.heading == heading
-    assert len(t.lines) == 4
+    assert t.position == pos0
+    assert t.heading == head0
 
 
 def test_forward_left_forward():
     t = Turtle()
+    pos0 = t.position
     d = 50
     a, sin_a = 60, (3**0.5) / 2
+    pos1 = pos0.translated(d + d / 2 / 2, -sin_a * d / 2)
     t.forward(d)
     t.left(a)
     t.forward(d / 2)
-    assert len(t.lines) == 2
-    p1 = Point(t.drawing.width // 2 + d, t.drawing.height // 2)
-    p2 = Point(p1.x + d / 2 / 2, p1.y - sin_a * d / 2)
-    assert t.lines[1].p1 == p1
-    assert t.lines[1].p2 == p2
+    assert t.heading == 360 - a
+    assert t.position == pos1
